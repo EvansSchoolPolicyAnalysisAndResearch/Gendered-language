edbda0860b36810a890eca45ca5b2ccfe5cbfe51
sjiayuanwang <sjwang171@gmail.com>
2023-07-22 12:02:42 -0700

    grambank code

--- code.R
+++ code.R
@@ -1,10 +1,10 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(readxl, writexl, dplyr, sp, rworldmap)
-languages <- read_excel("C:/Users/sphsc/OneDrive - UW/GitHub/Gendered-language/grambank data files/languages.xlsx")
-parameters <- read_excel("C:/Users/sphsc/OneDrive - UW/GitHub/Gendered-language/grambank data files/parameters.xlsx")
-values <- read_excel("C:/Users/sphsc/OneDrive - UW/GitHub/Gendered-language/grambank data files/values.xlsx")
+languages <- read_excel("C:/Users/sphsc/OneDrive - UW/GitHub/Gendered-language/grambank raw data/languages.xlsx")
+parameters <- read_excel("C:/Users/sphsc/OneDrive - UW/GitHub/Gendered-language/grambank raw data/parameters.xlsx")
+values <- read_excel("C:/Users/sphsc/OneDrive - UW/GitHub/Gendered-language/grambank raw data/values.xlsx")
 
-#Merge grammatical gender variable with language dataframe:
+##Merge grammatical gender variables (GB051, GB030) with language dataframe:
 #create GB051 subset (GB051=dummy variable for sex-based grammatical gender)
 values_GB051_only <- filter(values, 
                             values$Parameter_ID=="GB051") 
@@ -12,9 +12,36 @@ values_GB051_only <- subset(values_GB051_only,
                             select = -c(ID))
 names(values_GB051_only)[names(values_GB051_only) == 'Language_ID'] <- 'ID'
 lang_GB051 <- inner_join(languages, values_GB051_only)
-lang_GB051_na <- anti_join(languages, values_GB051_only) # these are excluded languages due to missing GB051 data
+lang_GB051_na <- anti_join(languages, values_GB051_only) # these are languages missing GB051 data
+names(lang_GB051)[names(lang_GB051) == 'Parameter_ID'] <- 'GB051'
+names(lang_GB051)[names(lang_GB051) == 'Value'] <- 'GB051_Value'
+names(lang_GB051)[names(lang_GB051) == 'Comment'] <- 'GB051_Comment'
+names(lang_GB051)[names(lang_GB051) == 'Source'] <- 'GB051_Source'
+names(lang_GB051)[names(lang_GB051) == 'Source_comment'] <- 'GB051_Source_comment'
+names(lang_GB051)[names(lang_GB051) == 'Coders'] <- 'GB051_Coders'
+lang_GB051 <- subset(lang_GB051, 
+                     select = -c(Code_ID))
+#create GB030 subset (GB030=dummy variable for gender distinction in third-person pronoun)
+values_GB030_only <- filter(values, 
+                            values$Parameter_ID=="GB030") 
+values_GB030_only <- subset(values_GB030_only, 
+                            select = -c(ID))
+names(values_GB030_only)[names(values_GB030_only) == 'Language_ID'] <- 'ID'
+lang_GB030 <- inner_join(languages, values_GB030_only)
+lang_GB030_na <- anti_join(languages, values_GB030_only) # these are languages missing GB030 data
+names(lang_GB030)[names(lang_GB030) == 'Parameter_ID'] <- 'GB030'
+names(lang_GB030)[names(lang_GB030) == 'Value'] <- 'GB030_Value'
+names(lang_GB030)[names(lang_GB030) == 'Comment'] <- 'GB030_Comment'
+names(lang_GB030)[names(lang_GB030) == 'Source'] <- 'GB030_Source'
+names(lang_GB030)[names(lang_GB030) == 'Source_comment'] <- 'GB030_Source_comment'
+names(lang_GB030)[names(lang_GB030) == 'Coders'] <- 'GB030_Coders'
+lang_GB030 <- subset(lang_GB030, 
+                    select = -c(Code_ID))
+#Join GB051 and GB030
+lang_GB051_GB030 <- inner_join(lang_GB051, lang_GB030)
+lang_GB051_GB030_na <- anti_join(lang_GB051, lang_GB030) # these have GB051 data but missing GB030 data
 
-#Add country variable based on language coordinates:
+##Add country variable based on language coordinates:
 lang_coord <- data.frame(languages$Longitude, languages$Latitude)
 row_coord_na <- which(is.na(lang_coord), arr.ind=TRUE) # these are rows with NA's in lang_coord
 lang_coord_na <- languages[row_coord_na[1:4,1], ] # data frame consisting of these rows
@@ -45,8 +72,10 @@ lang_coord_full_raw <- lang_coord_full
 lang_coord_full$country <- coords2country(lang_coord_full_raw)
 lang_coord_full$ISO3 <- coords2ISO3(lang_coord_full_raw)
 lang_coord_full$ID <- languages$ID #replicate ID column to lang_coord_full
-lang_GB051_country <- inner_join(lang_GB051, lang_coord_full)
-lang_GB051_country_filtered <- lang_GB051_country[!(lang_GB051_country$ID %in% lang_coord_na$ID),] #remove any row with fake coordinates
-gendlang <- subset(lang_GB051_country_filtered, 
+
+##Join datasets:
+lang_GB051_GB030_country <- inner_join(lang_GB051_GB030, lang_coord_full)
+lang_GB051_GB030_country_filtered <- lang_GB051_GB030_country[!(lang_GB051_GB030_country$ID %in% lang_coord_na$ID),] #remove any row with fake coordinates
+gendlang <- subset(lang_GB051_GB030_country_filtered, 
                    select = -c(languages.Longitude, languages.Latitude)) #remove duplicate coordinate columns to get final dataset
-write_xlsx(gendlang, 'C:/Users/sphsc/OneDrive/Documents/gendlang.xlsx') #create merged dataset in local drive
+write_xlsx(gendlang, 'C:/Users/sphsc/OneDrive - UW/GitHub/Gendered-language/grambank raw data/gendlang.xlsx') #create merged dataset in local drive
---
+++ code.do
@@ -0,0 +1,44 @@
+clear
+
+*Create .dta files for all raw data files
+import excel using "C:\Users\sphsc\OneDrive - UW\GitHub\Gendered-language\grambank raw data\languages.xlsx", firstrow clear
+save "C:\Users\sphsc\OneDrive - UW\GitHub\Gendered-language\grambank raw data\languages.dta"
+clear
+import excel using "C:\Users\sphsc\OneDrive - UW\GitHub\Gendered-language\grambank raw data\parameters.xlsx", firstrow clear
+save "C:\Users\sphsc\OneDrive - UW\GitHub\Gendered-language\grambank raw data\parameters.dta"
+clear
+import excel using "C:\Users\sphsc\OneDrive - UW\GitHub\Gendered-language\grambank raw data\values.xlsx", firstrow clear
+save "C:\Users\sphsc\OneDrive - UW\GitHub\Gendered-language\grambank raw data\values.dta"
+clear
+
+*Reshape values dataset long to wide
+use "C:\Users\sphsc\OneDrive - UW\GitHub\Gendered-language\grambank raw data\values.dta" 
+encode Parameter_ID, gen(Parameter_no)
+drop ID Parameter_ID
+reshape wide Value Code_ID Comment Source Source_comment Coders, i(Language_ID) j(Parameter_no)
+
+*optional: save reshaped values dataset with all parameters
+save "C:\Users\sphsc\OneDrive - UW\GitHub\Gendered-language\grambank raw data\values_reshaped.dta", replace
+
+*Save dataset with only parameters related to grammatical gender
+keep Language_ID Value10 Code_ID10 Comment10 Source10 Source_comment10 Coders10 Value25 Code_ID25 Comment25 Source25 Source_comment25 Coders25
+save "C:\Users\sphsc\OneDrive - UW\GitHub\Gendered-language\grambank raw data\values_gg.dta", replace
+clear
+
+*Join languages with grammatical gender parameters
+use "C:\Users\sphsc\OneDrive - UW\GitHub\Gendered-language\grambank raw data\languages.dta"
+rename ID Language_ID
+merge 1:1 Language_ID using "C:\Users\sphsc\OneDrive - UW\GitHub\Gendered-language\grambank raw data\values_gg.dta"
+
+* Find country using coordinates
+net get geo2xy, from("http://fmwww.bc.edu/repec/bocode/g")
+ssc install dataex
+ssc install geoinpoly
+geoinpoly Latitude Longitude using "geo2xy_world_coor.dta"
+merge m:1 _ID using "geo2xy_world_data.dta", ///
+    keep(master match) keepusing(geounit) nogen
+drop _merge country _ID
+rename geounit country
+
+*Save new dataset
+save "C:\Users\sphsc\OneDrive - UW\GitHub\Gendered-language\grambank raw data\gendlang.dta", replace
